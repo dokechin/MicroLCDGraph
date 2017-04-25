c33dab6f3fe4cadb33c67630fe2b742d59ef8fa5
dokechin <ttatsumi@ra2.so-net.ne.jp>
2017-04-25 19:35:31 +0900

    shift array implements

--- MicroLCDGraph.cpp
+++ MicroLCDGraph.cpp
@@ -15,14 +15,26 @@ void MicroLCDGraph::setDomain(int min, int max)
 
 void MicroLCDGraph::draw(*int data, byte width, byte height)
 {
-    int line[byte_size];
-    line[0] = 1;
+    byte line[byte_size];
+    line[0] = 0x01;
     for (int i=0;i< data_length;i++){
         int shift = byte_size * ( data[i] - min ) / (max - min);
-        line << shift;
+        shift_left(line, byte_size, shift);
         for(int j=0;j<byte_size;j++){
             bitmap[i + j* width] = line[j];
         }
     }
     lcd.draw(bitmap, width, height);
+}
+
+void shift_left(unsigned char *ar, int size, int shift)
+{
+    int carry = 0;
+    while (shift--) {
+        for (int i = size - 1; i >=0; --i) {
+            int next = (ar[i] & 0x80) ? 0x01 : 0;
+            ar[i] = carry | (ar[i] << 1);
+            carry = next;
+        }   
+    }
 }
